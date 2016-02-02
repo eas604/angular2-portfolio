0139c2df9b93b183e24709ba148c7819313d900a
Edwin Sheldon <edwin.sheldon@gmail.com>
2016-02-02 17:56:55 -0600

    add contact

---
+++ app/src/contact/contact.component.ts
@@ -0,0 +1,26 @@
+import {Component} from 'angular2/core';
+import {Http} from 'angular2/http'
+import {LangSvc} from '../lang/lang.svc'
+import {FromJSON} from '../utilities/fromJSON'
+import {Serializable} from '../utilities/utilities'
+
+class Link extends Serializable {
+    public title: string;
+    public url: string;
+}
+
+class ContactVM extends Serializable {
+    public title: string;
+    public links: Link[];
+}
+
+@Component({
+    selector: 'contact',
+    templateUrl: 'app/src/contact/contact.html'
+})
+export class ContactComponent extends FromJSON {
+
+    constructor(http: Http, lang: LangSvc) {
+        super(http, lang, 'app/src/contact/contact.json', new ContactVM());
+    }
+}
--- app/src/main/main.component.ts
+++ app/src/main/main.component.ts
@@ -8,6 +8,7 @@ import {AboutComponent} from '../about/about.component'
 import {ExperienceComponent} from '../experience/experience.component'
 import {EducationComponent} from '../education/education.component'
 import {ProjectsComponent} from '../projects/projects.component'
+import {ContactComponent} from '../contact/contact.component'
 
 
 @Component ({
@@ -19,7 +20,8 @@ import {ProjectsComponent} from '../projects/projects.component'
         AboutComponent, 
         ExperienceComponent,
         ProjectsComponent,
-        EducationComponent
+        EducationComponent,
+        ContactComponent
     ],
     providers: [HTTP_PROVIDERS, LangSvc]
 })
